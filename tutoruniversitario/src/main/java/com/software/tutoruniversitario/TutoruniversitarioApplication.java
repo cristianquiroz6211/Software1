e542792c523b9fc6ec47d5fe34933bfd0245acab
cristianquiroz6211 <Cristian.quiroz6211@uco.net.co>
2024-05-14 19:30:51 -0500

    Se crea el proyecto en java

---
+++ tutoruniversitario/src/main/java/com/software/tutoruniversitario/TutoruniversitarioApplication.java
@@ -0,0 +1,13 @@
+package com.software.tutoruniversitario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutoruniversitarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutoruniversitarioApplication.class, args);
+	}
+
+}
---
+++ tutoruniversitario/src/test/java/com/software/tutoruniversitario/TutoruniversitarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.software.tutoruniversitario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutoruniversitarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
